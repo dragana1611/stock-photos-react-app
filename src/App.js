b8b7af2d977bb99f7fa281491270016c46b51531
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-12 01:42:43 +0200

    added infinite scroll, search query

--- src/App.js
+++ src/App.js
@@ -1,47 +1,108 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [newImages, setNewImages] = useState(false);
+  const mounted = useRef(false);
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
       setLoading(false);
     } catch (error) {
+      setNewImages(false);
       setLoading(false);
     }
   };
-  useEffect(() => {
-    fetchImages();
-  }, []);
 
   useEffect(() => {
-    const event = window.addEventListener("scroll", () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 5
-      ) {
-        //-5px to start loading new images 5px earlier
-        console.log("it works");
-      }
-    });
-    return () => window.removeEventListener("scroll", event);
+    fetchImages();
+    // eslint-disable-next-line
+  }, [page]);
+
+  // useEffect(() => {
+  //   const event = window.addEventListener("scroll", () => {
+  //     if (
+  //       !loading &&
+  //       window.innerHeight + window.scrollY >= document.body.scrollHeight - 5
+  //     ) {
+  //       //-5px -> to start loading new images 5px earlier
+  //       console.log("it works");
+  //       setPage((oldPage) => {
+  //         return oldPage + 1;
+  //       });
+  //     }
+  //   });
+
+  //   return () => window.removeEventListener("scroll", event);
+  //   // eslint-disable-next-line
+  // }, []);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => oldPage + 1);
+    // eslint-disable-next-line
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 5) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", event);
+    return window.removeEventListener("scroll", event);
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // setPage(1);
+    // fetchImages();
+    if (!query) return;
+    if (page === 1) {
+      fetchImages(1);
+      return;
+    }
+    setPage(1);
   };
 
   return (
@@ -51,8 +112,8 @@ function App() {
           <input
             type="text"
             placeholder="search"
-            // value={query}
-            // onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             className="form-input"
           />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
--- src/Photo.js
+++ src/Photo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const Photo = ({
   urls: { regular },
@@ -11,7 +11,7 @@ const Photo = ({
   },
 }) => {
   return (
-    <article className="photo">
+    <article className='photo'>
       <img src={regular} alt={alt_description} />
       <div className='photo-info'>
         <div>
@@ -19,11 +19,11 @@ const Photo = ({
           <p>{likes} likes</p>
         </div>
         <a href={portfolio_url}>
-          <img src={medium} alt={name} className='user-img' />
+          <img src={medium} alt='' className='user-img' />
         </a>
       </div>
     </article>
-  );
-};
+  )
+}
 
-export default Photo;
+export default Photo
